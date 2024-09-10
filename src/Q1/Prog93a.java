b805bf695debddacdb31a9bf1684acca7887e483
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-09-10 11:24:07 -0500

    yummers

---
+++ src/Q1/Prog93a.java
@@ -0,0 +1,9 @@
+package Q1;
+import java.util.*;
+
+public class Prog93a {
+    public static void main(String[] args) {
+        double centsPerKilowattHour = 457;
+        double surcharge; // yeeeeehaaaawww
+    }
+}
