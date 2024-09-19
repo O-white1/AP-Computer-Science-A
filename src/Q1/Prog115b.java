5c51a08989d41f221d130587082ebf5cefcd284d
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-09-19 11:04:13 -0500

    prog115b done

---
+++ src/Q1/Prog115b.java
@@ -0,0 +1,9 @@
+package Q1;
+
+public class Prog115b {
+    public static void main(String[] args) {
+        for (int lcv = 0; lcv <= 36; lcv++) {
+            if (lcv % 2 == 0) System.out.printf("%d\n", lcv);
+        }
+    }
+}
