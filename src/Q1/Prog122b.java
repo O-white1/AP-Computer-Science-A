c18602202face64086c0414cde49e956f0517d34
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-09-19 11:08:47 -0500

    prog122b done

--- src/Q1/Prog122b.java
+++ src/Q1/Prog122b.java
@@ -2,6 +2,7 @@ package Q1;
 
 public class Prog122b {
     public static void main(String[] args) {
+        System.out.println("Hours:\tPay:");
         for (int lcv = 1; lcv <= 40; lcv++) {
             System.out.printf("%d\t%d\n", lcv, lcv*4);
         }
