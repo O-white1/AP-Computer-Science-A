99aa3987373c34f27f8524fa0a3e67641069f586
O-white1 <white.o3@stu.janesville.k12.wi.us>
2024-09-23 06:30:07 -0500

    Prog122c.java finsihed

---
+++ src/Q1/Prog122c.java
@@ -0,0 +1,13 @@
+package Q1;
+
+import java.io.PrintStream;
+
+public class Prog122c {
+        public static void main(String[] args) {
+            System.out.printf("%d    %d    %d    %d\n", 2, 3, 4, 4);
+            System.out.printf("%d    %d    %d    %d\n", 4, 5, 8, 16);
+            System.out.printf("%d    %d    %d    %d\n", 6, 7, 12, 36);
+            System.out.printf("%d    %d    %d    %d\n", 8, 9, 16, 64);
+            System.out.printf("%d    %d    %d    %d\n", 10, 11, 20, 100); // not sure why this program exists but i did what it asked. :)
+        }
+    }
