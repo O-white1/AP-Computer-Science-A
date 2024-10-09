7602dba4a50ad8c6abd0563b314632b4d263fffb
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-10-09 10:09:20 -0500

    Prog88aCl Done

--- src/Q1/Prog88aCl/Prog88a.java
+++ src/Q1/Prog88aCl/Prog88a.java
@@ -4,6 +4,28 @@ import java.util.Scanner;
 public class Prog88a {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Num1: ");
+        System.out.println("Enter Num1: "); int num1 = sc.nextInt();
+        System.out.println("Enter Num2: "); int num2 = sc.nextInt();
+
+        cl88a Cl = new cl88a(num1, num2);
+        Cl.Calc();
+        // :(
+        int    sum  = Cl.GetSum();
+        int    dif  = Cl.GetDif();
+        int    prod = Cl.GetProd();
+        int    dist = Cl.GetDist();
+        int    max  = Cl.GetMax();
+        int    min  = Cl.GetMin();
+        double avg  = Cl.GetAvg();
+
+        // D:
+        System.out.println("Sum: " + sum);
+        System.out.println("Dif: " + dif);
+        System.out.println("Prod: " + prod);
+        System.out.println("Dist: " + dist);
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
+        System.out.println("Avg: " + avg);
+
     }
 }
--- src/Q1/Prog88aCl/cl88a.java
+++ src/Q1/Prog88aCl/cl88a.java
@@ -1,5 +1,37 @@
 package Q1.Prog88aCl;
 
 public class cl88a {
+    private int n1;
+    private int n2;
+    private int sum, dif, prod, dist, max, min;
+    private double avg;
+
+    public cl88a(int num1, int num2) {
+        n1 = num1;
+        n2 = num2;
+        sum = 0;
+        dif = 0;
+        prod = 0;
+        dist = 0;
+        max = 0;
+        min = 0;
+    }
+    public void Calc() {
+        sum = n1 + n2;
+        dif = n1 - n2;
+        prod = n1 * n2;
+        dist = Math.abs(n2 - n1);
+        avg = ((double)n1 + n2) / 2;
+        if (n2 > n1) {max = n2; min = n1;}
+        else         {max = n1; min = n2;}
+    }
+    public int  GetSum() {return sum; }
+    public int  GetDif() {return dif; }
+    public int GetProd() {return prod;}
+    public int GetDist() {return dist;}
+    public int  GetMax() {return max; }
+    public int  GetMin() {return min; }
+    public double GetAvg() {return avg; }
+
 
 }
