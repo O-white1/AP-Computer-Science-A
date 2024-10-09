d49d79d5e55bccb19357fc8e4f315431d1b71181
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-10-09 11:25:13 -0500

    5-6 underway

--- src/Q1/lpFive_Six.java
+++ src/Q1/lpFive_Six.java
@@ -1,12 +1,13 @@
 package Q1;
 import java.util.Scanner;
 
-public class lpFive_Five {
+public class lpFive_Six {
     public static void main(String[] args) {
+        int sum;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter positive integer: "); String number = sc.nextLine();
         for (int lcv = 0; lcv < number.length(); lcv++) {
-            System.out.println(number.substring(lcv, lcv + 1)); // same as charAt(lcv)
+            sum +=number.substring(lcv, lcv + 1); // same as charAt(lcv)
         }
     }
 }
