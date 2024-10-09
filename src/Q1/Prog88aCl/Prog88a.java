878a91a2dfbba73c17cd759784c0c40d491456ca
O-white1 <white.o3@stu.janesville.k12.wi.us>
2024-10-08 21:00:49 -0500

    qweryt

---
+++ src/Q1/Prog88aCl/Prog88a.java
@@ -0,0 +1,9 @@
+package Q1.Prog88aCl;
+import java.util.Scanner;
+
+public class Prog88a {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Num1: ");
+    }
+}
---
+++ src/Q1/Prog88aCl/cl88a.java
@@ -0,0 +1,5 @@
+package Q1.Prog88aCl;
+
+public class cl88a {
+
+}
