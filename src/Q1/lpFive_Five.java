2dfd94a0281bd2f2a62f2942ff2cef4566811837
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-10-29 11:18:38 -0500

    qwerty

--- src/Q1/lpFive_Five.java
+++ src/Q1/lpFive_Five.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 public class lpFive_Five {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter positive integer: "); String number = sc.nextLine();
+
+        System.out.println("Enter positive integer: ");
+        String number = sc.nextLine();
+
         for (int lcv = 0; lcv < number.length(); lcv++) {
-            System.out.println(number.substring(lcv, lcv + 1)); // same as charAt(lcv)
+            System.out.println(number.substring(lcv, lcv+1)); // same as charAt(lcv)
         }
     }
 }
