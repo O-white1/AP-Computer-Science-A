09cf6c5f2d22f56be88d75ed06063ed4f4280944
O-white1 <white.o3@stu.janesville.k12.wi.us>
2024-11-02 10:44:28 -0500

    lpFive_Six.java complete

--- src/Q1/lpFive_Six.java
+++ src/Q1/lpFive_Six.java
@@ -5,10 +5,11 @@ public class lpFive_Six {
     public static void main(String[] args) {
         int sum = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter positive integer: "); String number = sc.nextLine();
-        for (int lcv = 0; lcv < number.length(); lcv++) {
-            //sum += number.substring(lcv, lcv + 1); // same as charAt(lcv)
-
+        System.out.println("Enter a non-negative integer: "); String num = sc.nextLine();
+        for (int lcv = 0; lcv < num.length(); lcv++) {
+            sum += Character.getNumericValue(num.charAt(lcv));
         }
+        System.out.println("Sum of digits: " + sum);
+        sc.close();
     }
 }
