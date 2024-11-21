377a3f6ea3258d6a9494d60023d8fc4cd95fbb91
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-11-21 11:22:23 -0600

    msoe 2016 2

---
+++ src/Q2/MSOE_2016_2.java
@@ -0,0 +1,17 @@
+package Q2;
+
+import java.util.Scanner;
+
+public class MSOE_2016_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Length of Side A"); double sideA  = sc.nextDouble();
+        System.out.println("Enter Length of Side C"); double sideC  = sc.nextDouble();
+        System.out.println("Enter Angle of Side A");  double angleA = sc.nextDouble();
+
+        double angleC = Math.sin(sideC) / (Math.sin(sideA) / angleA);
+        System.out.printf("The Degree angle of C is %.2f", angleC);
+
+
+    }
+}
