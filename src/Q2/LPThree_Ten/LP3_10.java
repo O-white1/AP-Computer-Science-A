0bf0f6b84f4f4cb8c5f79d063d4335211cc6f9a3
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-12-06 09:55:40 -0600

    lp3-9

--- src/Q2/LPThree_Ten/LP3_10.java
+++ src/Q2/LPThree_Ten/LP3_10.java
@@ -1,7 +1,7 @@
 package Q2.LPThree_Ten;
 import java.util.Scanner;
 
-public class LP3_9 {
+public class LP3_10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Number of Burgers: "); int burgers = sc.nextInt();
@@ -12,6 +12,7 @@ public class LP3_9 {
         goofJuice.calc();
 
         double Bcost = goofJuice.getBurg();
+
         double Fcost = goofJuice.getFri();
         double Scost = goofJuice.getSoda();
         double total = goofJuice.getTotal();
--- src/Q2/LPThree_Ten/clLp3_10.java
+++ src/Q2/LPThree_Ten/clLp3_10.java
@@ -4,28 +4,25 @@ public class clLp3_10 {
     private int Burgers;
     private int Fries;
     private int Sodas;
+    double      BurgersCost = 0;
+    double      FriesCost   = 0;
+    double      SodasCost   = 0;
 
     public clLp3_10(int b, int f, int s) {
         Burgers = b;
         Fries = f;
         Sodas = s;
+        BurgersCost = b;
+        FriesCost   = f;
+        SodasCost   = s;
     }
-
     public void calc() {
-        Burgers *= 1.69;
-        Fries   *= 1.09;
-        Sodas   *= 0.99;
+        BurgersCost *= 1.69;
+        FriesCost   *= 1.09;
+        SodasCost   *= 0.99;
     }
-    public double getBurg() {
-        return Burgers;
-    }
-    public double getFri() {
-        return Fries;
-    }
-    public double getSoda() {
-        return Sodas;
-    }
-    public double getTotal() {
-        return Burgers + Fries + Sodas;
-    }
+    public double  getBurg()  {return BurgersCost;}
+    public double   getFri()   {return FriesCost;  }
+    public double  getSoda()  {return SodasCost;  }
+    public double getTotal() {return BurgersCost + FriesCost + SodasCost;}
 }
