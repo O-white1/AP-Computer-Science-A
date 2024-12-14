d1cac67edf5ae90cbb2d93a3891adce3629a1ad9
O-white1 <white.o3@stu.janesville.k12.wi.us>
2024-12-14 16:58:23 -0600

    Prog215h Complete 12/14/2024

--- src/Q2/Prog215h/Contestant.java
+++ src/Q2/Prog215h/Contestant.java
@@ -3,20 +3,24 @@ package Q2.Prog215h;
 import java.util.ArrayList;
 
 public class Contestant {
-    public double[] myScores;
+    private double[] myScores;
     private double sum;
     private double avg;
 
-    public Contestant() {
+    public Contestant(double[] scores) {
         sum = 0;
         avg = 0;
+        myScores = scores;
     }
 
     public void calc() {
-        for (double x:myScores)sum += x;
+        for (double x :myScores) sum += x;
         avg = sum/myScores.length;
     }
-    public double getAverage() {return avg;}
-    public double[] getScores() {return myScores;}
-
+    public void print() {
+        System.out.println("\nScores: ");
+        for (double score : myScores)
+            System.out.print("\t" + score + " ");
+        System.out.println("\nAverage: " + avg);
+    }
 }
--- src/Q2/Prog215h/Prog215h.java
+++ src/Q2/Prog215h/Prog215h.java
@@ -1,5 +1,6 @@
 package Q2.Prog215h;
 
+import javax.print.attribute.standard.PrinterMakeAndModel;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -8,20 +9,18 @@ import java.util.Scanner;
 public class Prog215h {
     public static void main(String[] args) {
         try {
-            Scanner file = new Scanner(new File("langdat/prog215h.txt"));
-            ArrayList<Double> allScores = new ArrayList<>();
-            Contestant Alice = new Contestant();
+            Scanner file = new Scanner(new File("Langdat/prog215h.txt"));
+            ArrayList<Contestant> contestants = new ArrayList<>();
 
-            while (file.hasNext())
-                allScores.add(file.nextDouble());
-
-            //TODO: all 6 contestants
-            for (int lcv=0; lcv<=8; lcv++) {
-                Alice.myScores[lcv] = allScores.get(lcv);
+            while (file.hasNext()) {
+                double[] scores = new double[8];
+                for (int i = 0; i < 8; i++)
+                    scores[i] = file.nextDouble();
+                Contestant c = new Contestant(scores);
+                c.calc();
+                contestants.add(c);
             }
-
-            Alice.calc();
-
+            for (Contestant c : contestants) c.print();
         }
         catch(IOException e) {
             System.out.println("Error: " + e);
