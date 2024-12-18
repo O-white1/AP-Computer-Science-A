740f3dbcb3bd2c50ebd46e640a1305fa46309bd2
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-12-18 11:35:58 -0600

    12/16/2024

---
+++ src/Q2/Project2/Item.java
@@ -0,0 +1,14 @@
+package Q2.Project2;
+
+
+public class Item {
+    String myName;
+    String myDate;
+    int myPriority;
+
+    public Item(String name, String date, int priority) {
+        myName = name;
+        myDate = date;
+        myPriority = priority;
+    }
+}
