02ea09dc38753f8fd70426c1b8679be1c63e03b6
Owen White <white.o3@stu.janesville.k12.wi.us>
2025-01-17 10:08:09 -0600

    465b Done

--- src/Q2/Prog465b.java
+++ src/Q2/Prog465b.java
@@ -7,13 +7,20 @@ import java.util.Scanner;
 public class Prog465b {
     public static void main(String[] args) {
         try {
-            Scanner file = new Scanner(new File("Langdat/Prog465b.dat"));
+            Scanner file = new Scanner(new File("Langdat/prog465b.dat"));
             int[][] mat = new int[3][4];
+
+            for (int r = 0; r < 3; r++) {
+                for (int c = 0; c < 4; c++) {
+                    mat[r][c] = file.nextInt();
+                    System.out.print(mat[r][c] + "\t\t");
+                }
+            System.out.println();
+            }
+            System.out.println("Row:\tColumn:");
             for (int r = 0; r < 3; r++)
                 for (int c = 0; c < 4; c++)
-                    mat[r][c] = file.nextInt();
-
-
+                    if (mat[r][c] < 100) System.out.printf("%d\t\t\t%d\n", r + 1, c + 1);
         }
         catch(IOException e) {
             System.out.println("Error: " + e);
