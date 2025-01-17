7c8c6ff7688715e17a785e2fb5faf5ea1934df57
Owen White <white.o3@stu.janesville.k12.wi.us>
2025-01-17 10:26:29 -0600

    465c Done

--- src/Q2/Prog465c.java
+++ src/Q2/Prog465c.java
@@ -9,12 +9,15 @@ public class Prog465c {
         try {
             Scanner file = new Scanner(new File("Langdat/prog465c.txt"));
             int[][] mat  = new int[file.nextInt()][file.nextInt()];
+
             System.out.println("Original File: ");
-            for (int i = 0; i < mat.length; i++)
+            for (int i = 0; i < mat.length; i++) {
                 for (int j = 0; j < mat[i].length; j++) {
                     mat[i][j] = file.nextInt();
                     System.out.println(mat[i][j]);
                 }
+            }
+
             System.out.println();
             System.out.println("Matrix: ");
             for (int row = 0; row < mat.length; row++) {
