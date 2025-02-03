924d2f975804438ebf0aa9c07c74089751020de7
Owen White <white.o3@stu.janesville.k12.wi.us>
2025-02-03 11:41:17 -0600

    1/28/25 477a not done

--- src/Q3/Prog477a/Prog477a.java
+++ src/Q3/Prog477a/Prog477a.java
@@ -10,39 +10,43 @@ public class Prog477a {
         try {
             Scanner file = new Scanner(new File("Langdat/prog477a.dat"));
             ArrayList<Student> list = new ArrayList<>();
-            int[][] mat = new int[3][5];
+            int[][] mat = new int[6][3];
 
-
-            while (file.hasNext()) {
-                Student s = new Student(file.nextInt(), file.nextInt());
-                list.add(s);
-            }
+            while (file.hasNext())
+                list.add(new Student(file.nextInt(), file.nextInt()));
             for (Student s : list) {
-                if (s.getSex()==1) {
-                    if ((s.getGrade())==1) mat[0][0]++;
-                    if ((s.getGrade())==2) mat[1][0]++;
-                    if ((s.getGrade())==3) mat[2][0]++;
-                    if ((s.getGrade())==4) mat[3][0]++;
-                    if ((s.getGrade())==5) mat[4][0]++;
-                }
-                if (s.getSex()==2) {
-                    if ((s.getGrade())==1) mat[0][1]++;
-                    if ((s.getGrade())==2) mat[1][1]++;
-                    if ((s.getGrade())==3) mat[2][1]++;
-                    if ((s.getGrade())==4) mat[3][1]++;
-                    if ((s.getGrade())==5) mat[4][1]++;
-                }
-                for (int i = 0; i < mat.length; i++) {
-                    for (int j = 0; j < mat[0].length; j++) {
-                        System.out.print(mat[i][j] + "\t");
-                    }
-                    System.out.println();
-                }
+                if (s.getSex()==1)
+                    for (int i = 0; i <= 5; i++)
+                        if (s.getGrade()==i){
+                            mat[i][0]++;
+                            mat[i][2]++;
+                        }
 
+                if (s.getSex()==2)
+                    for (int i = 0; i <= 5; i++)
+                        if (s.getGrade()==i){
+                            mat[i][1]++;
+                            mat[i][2]++;
+                        }
             }
+            System.out.println("Grade\t\tMale\t\tFemale\t\tTotal");
+            for (int i = 0; i < mat.length; i++) {
+                switch (i) {
+                    //case 1: System.out.print("A\t");   break;
+                    case 2: System.out.print("B\t");   break;
+                    case 3: System.out.print("C\t");   break;
+                    case 4: System.out.print("D\t");   break;
+                    case 5: System.out.print("F\t"); break;
+                    //default: System.out.print("\t");
+                    default: System.out.print("A\t");   break;
+                }
 
-
-
+                int[] arr = mat[i];
+                for (int j : arr) {
+                    System.out.print("\t\t" + j + "\t");
+                }
+                System.out.println();
+            }
         }
         catch(IOException e) {
             System.out.println("Error: " + e);
