b2ac2760cd9d4fbb5f6bab6905065039075c53ad
Owen White <white.o3@stu.janesville.k12.wi.us>
2025-02-06 10:14:00 -0600

    2/6/2025 465h complete

--- src/Q3/Prog465h.java
+++ src/Q3/Prog465h.java
@@ -2,7 +2,6 @@ package Q3;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class Prog465h {
@@ -10,25 +9,38 @@ public class Prog465h {
         try {
             Scanner file = new Scanner(new File("Langdat/prog465h.dat"));
             while (file.hasNext()) {
+                int count = 0;
                 int rows = file.nextInt();
                 int cols = file.nextInt();
                 int[][] mat = new int[rows][cols];
-                int[][] red = new int[3][cols];
 
-                for (int i = 1; i <= rows; i++)
-                    for (int j = 1; j <= cols; j++)
-                        mat[i][j] = file.nextInt();
-
-                for (int r = 0; r <= rows; r++) {
-                    for (int c = 0; c < cols; c++) {
-                        if (mat[r][c] != 0) {
-                            //TODO: populate reduced matrix
-                        }
+                for (int r = 0; r < mat.length; r++) {
+                    for (int c = 0; c < mat[0].length; c++) {
+                        mat[r][c] = file.nextInt();
+                        System.out.print(mat[r][c] + " ");
+                        if (mat[r][c] != 0)
+                            count++;
                     }
                     System.out.println();
                 }
+                System.out.println("\n\n");
+
+
+                int[][] red = new int[3][count+1];
+                for (int r = 0; r < mat.length; r++) {
+                    for (int c = 0; c < mat[0].length; c++) {
+                        if (mat[r][c] != 0) {
+                            red[0][count] = r + 1;
+                            red[1][count] = c + 1;
+                            red[2][count] = mat[r][c];
+                            System.out.println(red[0][count] + " " + red[1][count] + " " + red[2][count]);
+                        }
+                    }
+                }
                 System.out.println("\n");
             }
+
+
         }
         catch(IOException e) {
             System.out.println("Error: " + e);
