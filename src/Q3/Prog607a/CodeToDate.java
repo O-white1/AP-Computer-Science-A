5d729fec73e96b02b9a78e2d59730f7da5bbf898
Owen White <white.o3@stu.janesville.k12.wi.us>
2025-02-17 11:25:08 -0600

    2/17/2025 607a underway

--- src/Q3/Prog607a/CodeToDate.java
+++ src/Q3/Prog607a/CodeToDate.java
@@ -2,6 +2,7 @@ package Q3.Prog607a;
 
 public class CodeToDate {
     char[] alph = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
     String myCode;
     String myDate;
 
