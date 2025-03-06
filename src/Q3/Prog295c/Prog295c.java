76766b225f6fe2a111b11d630e399c2fec8520f0
Owen White <white.o3@stu.janesville.k12.wi.us>
2025-03-06 11:25:19 -0600

    Prog295c TODO

--- src/Q3/Prog295c/Prog295c.java
+++ src/Q3/Prog295c/Prog295c.java
@@ -4,21 +4,33 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Prog295c {
     public static void main(String[] args){
         try {
-        Scanner file = new Scanner(new File("C:\\Users\\white.o3\\Documents\\GitHub\\AP-Comp-Sci-A\\Langdat\\merge1.dat"));
-        ArrayList<Employee> people = new ArrayList<>();
+        Scanner file1 = new Scanner(new File("Langdat/merge1.dat"));
+        Scanner file2 = new Scanner(new File("Langdat/merge2.dat"));
+        Stack<Employee> file1Stack = new Stack<>();
+        Stack<Employee> file2Stack = new Stack<>();
 
-        while (file.hasNext()) {
-            String line = file.nextLine();
+        while (file1.hasNext()) {
+            String line = file1.nextLine();
 
             String SSN = line.substring(0, 13);
             char classification = line.charAt(15);
             char type = line.charAt(16);
             int dptCode = Integer.parseInt(line.substring(17));
-            people.add(new Employee(SSN, classification, type, dptCode));
+            file1Stack.push(new Employee(SSN, classification, type, dptCode));
+        }
+        while (file2.hasNext()) {
+            String line = file1.nextLine();
+
+            String SSN = line.substring(0, 13);
+            char classification = line.charAt(15);
+            char type = line.charAt(16);
+            int dptCode = Integer.parseInt(line.substring(17));
+            file2Stack.push(new Employee(SSN, classification, type, dptCode));
         }
 
         }
@@ -27,4 +39,11 @@ public class Prog295c {
             }
         }
     }
-}
+/*
+000-00-0001   A      B     1
+111-11-1111   C      B     1
+123-45-6789   A      F     5
+222-22-2227   A      L     9
+321-01-2345   A      B     1
+
+ */
