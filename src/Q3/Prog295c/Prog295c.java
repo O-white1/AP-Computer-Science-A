ca3ceeabf386395bae8a6351e3606067942dcae2
Owen White <white.o3@stu.janesville.k12.wi.us>
2025-04-07 10:49:25 -0500

    Prog295c TODO

--- src/Q3/Prog295c/Prog295c.java
+++ src/Q3/Prog295c/Prog295c.java
@@ -13,24 +13,34 @@ public class Prog295c {
         Scanner file2 = new Scanner(new File("Langdat/merge2.dat"));
         Stack<Employee> file1Stack = new Stack<>();
         Stack<Employee> file2Stack = new Stack<>();
+        Stack<Employee> allPeople  = new Stack<>();
 
         while (file1.hasNext()) {
-            String line = file1.nextLine();
+            String line = file2.nextLine();
 
-            String SSN = line.substring(0, 13);
-            char classification = line.charAt(15);
-            char type = line.charAt(16);
-            int dptCode = Integer.parseInt(line.substring(17));
-            file1Stack.push(new Employee(SSN, classification, type, dptCode));
+            String SSN = line.substring(0, 10);
+            char classification = line.charAt(12);
+            char type = line.charAt(14);
+            int dptCode = Integer.parseInt(line.substring(15));
+            file2Stack.push(new Employee(SSN, classification, type, dptCode));
         }
         while (file2.hasNext()) {
-            String line = file1.nextLine();
+            String line = file2.nextLine();
 
-            String SSN = line.substring(0, 13);
-            char classification = line.charAt(15);
-            char type = line.charAt(16);
-            int dptCode = Integer.parseInt(line.substring(17));
+            String SSN = line.substring(0, 10);
+            char classification = line.charAt(12);
+            char type = line.charAt(14);
+            int dptCode = Integer.parseInt(line.substring(15));
             file2Stack.push(new Employee(SSN, classification, type, dptCode));
+        }
+        for (int i = 0; i < file1Stack.size(); i++) {
+            if (Integer.parseInt(file1Stack.get(i).getSSN()) >= Integer.parseInt(file2Stack.get(i).getSSN())) allPeople.push(file1Stack.get(i));
+            else allPeople.push(file2Stack.get(i));
+        }
+
+        for (int i = 0; i < allPeople.size()-1; i++) {
+            Employee e = allPeople.get(i);
+            System.out.println(e.getSSN() + "\t" + e.getClassification() + "\t" + e.getType() + "\t" + e.getDepartmentCode());
         }
 
         }
