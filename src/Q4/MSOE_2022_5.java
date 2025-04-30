3f8774a337a9a6fa3e4a3b7ee0064a7c19019d7b
Owen White <white.o3@stu.janesville.k12.wi.us>
2025-04-30 11:07:11 -0500

    4/30/2025: Update

--- src/Q4/MSOE_2022_5.java
+++ src/Q4/MSOE_2022_5.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class MSOE_2022_5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Weight of Shell 1: "); int weight1 = sc.nextInt();
-        System.out.println("Enter Weight of Shell 2: "); int weight2 = sc.nextInt();
-        System.out.println("Enter Weight of Shell 3: "); int weight3 = sc.nextInt();
-        System.out.println("The minimum number of buckets is "+ (int)Math.ceil((weight1 + weight2 + weight3)/100.0));
+        int sum = 0;
+        for (int lcv=0;lcv<3;lcv++) {
+            System.out.printf("Enter Weight of Shell %d: \n", lcv+1); sum+= sc.nextInt();
+        }
+        System.out.println("The minimum number of buckets is "+ (int)Math.ceil((sum)/100.0));
     }
 }
